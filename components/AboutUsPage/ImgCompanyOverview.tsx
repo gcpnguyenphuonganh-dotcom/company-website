b25c7f8d133fec8e0826c518e2da937f1838975b
diamondzve-ops <diamondzve@gmail.com>
2026-07-22 13:27:32 +0700

    Update: ảnh overview aboutus

--- components/AboutUsPage/ImgCompanyOverview.tsx
+++ components/AboutUsPage/ImgCompanyOverview.tsx
@@ -10,7 +10,7 @@ const PHOTOS = [
   '/AboutUs/CompanyOverview/3.png',
   '/AboutUs/CompanyOverview/4.png',
   '/AboutUs/CompanyOverview/5.png',
-  '/AboutUs/CompanyOverview/6.png',
+  '/AboutUs/AboutUs.png',
   '/AboutUs/CompanyOverview/7.png',
   '/AboutUs/CompanyOverview/8.png',
 ];
